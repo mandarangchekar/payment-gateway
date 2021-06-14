4f22dd29c7875507516cca829e11c6daab5d6b6d
Mandar Angchekar <65801031+mandarangchekar@users.noreply.github.com>
2021-06-14 22:14:08 +0530

    Update config.js

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,6 +1,6 @@
 var PaytmConfig = {
-  mid: "Vdjgzd05444888877713",
-  key: "X0NP%5JfJ8gsNpm9",
-  website: "DEFAULT",
+//   mid: "Vdjgzd05444888877713",
+//   key: "X0NP%5JfJ8gsNpm9",
+//   website: "DEFAULT",
 };
 module.exports.PaytmConfig = PaytmConfig;
